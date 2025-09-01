c90123eabffe8dad3a49599e45bebb46615a098e
caiquegabriel1604 <caique.braga.brito@escola.pr.gov.br>
2025-09-01 07:46:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ function imc(){
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
-    const resultado = document.getElementById('resultado').value;
+    const resultado = document.getElementById
+    ('resultado');
 
-    const valorIMC = (peso/(altura*altura)).toFixed
+    const valorIMC = (peso/(altura*altura)).toFixed(1);
 
     resultado.textContent = `${nome} seu IMC é ${valorIMC}!`;
 }
